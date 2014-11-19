e9e215e4c9ea93a77669fe64753b324ff3b82df6
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-11-19 12:57:20 +0100

    Wilks Correlation Ratio working

--- emetrics/coefficients/correlation_ratio.py
+++ emetrics/coefficients/correlation_ratio.py
@@ -1,7 +1,29 @@
+import numpy
+
 __author__ = 'Emanuele Tamponi'
 
 
 class CorrelationRatio(object):
 
     def __call__(self, inputs, labels):
-        return 1 - 0.154
+        classes = numpy.unique(labels)
+        feature_num = inputs.shape[1]
+        class_means = numpy.zeros((len(classes), feature_num))
+        class_sizes = numpy.zeros(len(classes))
+        split_inputs = []
+        for i, c in enumerate(classes):
+            split_inputs.append(inputs[labels == c])
+            class_means[i] = split_inputs[-1].mean(axis=0)
+            class_sizes[i] = len(split_inputs[-1])
+        mean_input = inputs.mean(axis=0)
+        b_matrix = numpy.zeros((feature_num, feature_num))
+        for class_mean, class_size in zip(class_means, class_sizes):
+            class_shift = (class_mean - mean_input).reshape((feature_num, 1))
+            b_matrix += class_size * numpy.dot(class_shift, class_shift.transpose())
+        w_matrix = numpy.zeros((feature_num, feature_num))
+        for class_mean, class_inputs in zip(class_means, split_inputs):
+            for x in class_inputs:
+                shift = (x - class_mean).reshape((feature_num, 1))
+                w_matrix += numpy.dot(shift, shift.transpose())
+        wilks_lambda = numpy.linalg.det(w_matrix) / numpy.linalg.det(w_matrix + b_matrix)
+        return 1 - wilks_lambda
