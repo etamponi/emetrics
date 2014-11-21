88030c2d84b0d61078815dd82fee38f13a8f79d1
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-11-21 10:56:30 +0100

    Fix names and use with instead of opening without closing...

--- emetrics/scores/_aggregation_score_test.py
+++ emetrics/scores/_aggregation_score_test.py
@@ -8,20 +8,22 @@ __author__ = 'Emanuele Tamponi'
 
 class AggregationScoreTest(unittest.TestCase):
 
-    def test_correlation_ratio_on_dataset(self):
-        data = arff.load(open('test_files/aggregation_score_dataset.arff'))
-        X = numpy.asarray([row[:-1] for row in data["data"]])
-        y = numpy.asarray([row[-1] for row in data["data"]])
-        cr = AggregationScore()
-        self.assertAlmostEqual(1 - 0.154, cr(X, y), places=3, msg="Wilks Correlation Ratio not working")
+    def test_score_on_dataset(self):
+        # Result taken from Rencher's book Methods for Multivariate Analysis
+        with open('test_files/aggregation_score_dataset.arff') as f:
+            data = arff.load(f)
+            X = numpy.asarray([row[:-1] for row in data["data"]])
+            y = numpy.asarray([row[-1] for row in data["data"]])
+            cr = AggregationScore()
+            self.assertAlmostEqual(1 - 0.154, cr(X, y), places=3, msg="Wilks Correlation Ratio not working")
 
-    def test_correlation_ratio_zero_cr(self):
+    def test_score_is_zero(self):
         X = numpy.ones((10, 2))
         y = numpy.random.choice(["a", "b"], size=10)
         cr = AggregationScore()
         self.assertEqual(0, cr(X, y), msg="CR should be 0, got {}".format(cr(X, y)))
 
-    def test_correlation_ratio_one_cr(self):
+    def test_score_is_one(self):
         X = numpy.ones((10, 2))
         X[:5, :] = 0
         y = ["a"] * 5 + ["b"] * 5
