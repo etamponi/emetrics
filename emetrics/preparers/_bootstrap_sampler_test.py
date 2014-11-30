aab2b1df9c1ffd29fc811ee04b3c17836d29ddbb
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-11-30 19:16:44 +0100

    BootstrapSampler with custom percents

--- emetrics/preparers/_bootstrap_sampler_test.py
+++ emetrics/preparers/_bootstrap_sampler_test.py
@@ -16,3 +16,13 @@ class BootstrapSamplerTest(unittest.TestCase):
         for row in X_bootstrap:
             self.assertIn(row, X)
         numpy.testing.assert_array_equal(X.shape, X_bootstrap.shape)
+
+    def test_bootstrap_specified_percent(self):
+        X = numpy.random.rand(20, 3)
+        y = numpy.random.choice(["a", "b", "c"], size=20)
+        sampler = BootstrapSampler(sampling_percent=150)
+        X_bootstrap = sampler.apply(X, y)
+        self.assertFalse(numpy.all(X == X_bootstrap))
+        for row in X_bootstrap:
+            self.assertIn(row, X)
+        numpy.testing.assert_array_equal([30, 3], X_bootstrap.shape)
--- emetrics/preparers/bootstrap_sampler.py
+++ emetrics/preparers/bootstrap_sampler.py
@@ -5,6 +5,9 @@ __author__ = 'Emanuele Tamponi'
 
 class BootstrapSampler(object):
 
+    def __init__(self, sampling_percent=100):
+        self.sampling_percent = float(sampling_percent) / 100
+
     def apply(self, inputs, labels):
-        indices = numpy.random.choice(len(inputs), size=len(inputs))
+        indices = numpy.random.choice(len(inputs), size=int(self.sampling_percent*len(inputs)))
         return inputs[indices]
