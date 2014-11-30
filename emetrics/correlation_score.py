192fa7173450e03e66592df753e8f0d8e5b08832
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-11-30 19:41:27 +0100

    Add preparer pipeline

--- emetrics/correlation_score.py
+++ emetrics/correlation_score.py
@@ -9,11 +9,20 @@ __author__ = 'Emanuele Tamponi'
 
 class CorrelationScore(object):
 
-    def __init__(self, coefficient=AssociationMeasure(), label_encoder=OrdinalLabelEncoder()):
+    def __init__(self,
+                 coefficient=AssociationMeasure(),
+                 preparer_pipeline=None,
+                 label_encoder=OrdinalLabelEncoder()):
         self.coefficient = coefficient
+        if preparer_pipeline is None:
+            self.preparer_pipeline = []
+        else:
+            self.preparer_pipeline = preparer_pipeline
         self.label_encoder = label_encoder
 
     def __call__(self, inputs, labels):
+        for preparer in self.preparer_pipeline:
+            inputs = preparer.apply(inputs, labels)
         encoded_labels = self.label_encoder(labels)
         values = numpy.zeros(encoded_labels.shape[1])
         for i, y in enumerate(encoded_labels.T):
