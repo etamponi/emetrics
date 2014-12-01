c02389e089dadff67c49b9bc27c5a07c5a0476ef
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-12-01 16:50:01 +0100

    Parallel version

--- emetrics/evaluation/all_experiments.py
+++ emetrics/evaluation/all_experiments.py
@@ -1,10 +1,14 @@
 from itertools import product
 import cPickle
+import multiprocessing
 import os
+import signal
+from time import sleep
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble.bagging import BaggingClassifier
 from sklearn.ensemble.weight_boosting import AdaBoostClassifier
+import sys
 
 from emetrics.coefficients import AssociationMeasure
 from emetrics.coefficients.determination_coefficient import DeterminationCoefficient
@@ -21,22 +25,33 @@ __author__ = 'Emanuele Tamponi'
 
 
 def main():
+    datasets = dataset_names()
+    should_normalize = [True, False]
     # All sizes up to 10, then 15, 20, 25, 30 and 35
     subset_sizes = range(1, 11) + range(15, 36, 5)
-    datasets = dataset_names(n_groups=2, group=0)
-    for dataset, normalize, subset_size in list(product(datasets, [True, False], subset_sizes)):
-        results_file_name = get_results_file_name(dataset, normalize, subset_size)
-        print "Running {} with {} features{}".format(dataset, subset_size, " and normalization" if normalize else ""),
-        if os.path.isfile("results/{}.res".format(results_file_name)):
-            print "--- experiment already ran, continuing..."
-            continue
-        results = get_experiment(dataset, subset_size, normalize).run()
-        if results is not None:
-            with open("results/{}.res".format(results_file_name), "w") as f:
-                cPickle.dump(results, f)
-            print ""
-        else:
-            print "\r",
+
+    pool = multiprocessing.Pool(processes=4)
+    signal.signal(signal.SIGINT, terminate_pool(pool))
+
+    results = pool.map_async(run_parallel, product(datasets, should_normalize, subset_sizes))
+    results.get(24*3600*1000)  # Wait for 1000 days
+    pool.join()
+
+
+def run_parallel(params):
+    dataset, normalize, subset_size = params
+    results_file_name = get_results_file_name(dataset, normalize, subset_size)
+    print "starting experiment {}".format(results_file_name)
+    if os.path.isfile("results/{}.res".format(results_file_name)):
+        print "results of {} already present.".format(results_file_name)
+        return
+    results = get_experiment(dataset, subset_size, normalize).run()
+    if results is not None:
+        with open("results/{}.res".format(results_file_name), "w") as f:
+            cPickle.dump(results, f)
+        print "results of {} saved.".format(results_file_name)
+    else:
+        print "experiment {} not available (subset_size >= n_features)".format(results_file_name)
 
 
 def get_experiment(dataset_name, subset_size, normalize):
@@ -85,6 +100,15 @@ def get_results_file_name(dataset, normalize, subset_size):
         "norm" if normalize else "orig",
         subset_size
     )
+
+
+def terminate_pool(pool):
+    def signal_handler(sig, frame):
+        print "Terminating..."
+        pool.terminate()
+        pool.join()
+        sys.exit(0)
+    return signal_handler
 
 
 if __name__ == "__main__":
