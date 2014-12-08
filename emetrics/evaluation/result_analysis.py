c21dc571ebf468d1e253347e8e3c7dea5b4f9bf3
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-12-08 18:36:21 +0100

    Plot correlations

--- emetrics/evaluation/result_analysis.py
+++ emetrics/evaluation/result_analysis.py
@@ -15,7 +15,9 @@ from emetrics.evaluation.all_experiments import get_results_file_name
 __author__ = 'Emanuele Tamponi'
 
 
-STYLES = ["bo--", "r^-.", "gs:", "cp-.", "mD--"]
+SCATTER_COLORS = ["b", "r", "g", "k", "m"]
+SCATTER_MARKERS = ["o", "^", "s", "x", "D"]
+LINE_STYLES = ["b--", "r-.", "g:", "k-.", "m--"]
 LEGEND = {
     "ab": "AdaBoost",
     "gb": "Gradient Boosting",
@@ -46,6 +48,7 @@ def main():
         all_results[(dataset, normalize, subset_size)] = prepare_results(results)
 
     plot_correlation_to_range(all_results, pyplot)
+    # plot_correlation(all_results, pyplot)
 
 
 def configure_matplotlib():
@@ -71,22 +74,22 @@ def configure_matplotlib():
     matplotlib.rcParams.update(pgf_rc)
 
 
-def prepare_results(results):
-    scorers = sorted(results["scores"].keys())
-    classifiers = sorted(results["errors"].keys())
+def prepare_results(raw_results):
+    scorers = sorted(raw_results["scores"].keys())
+    classifiers = sorted(raw_results["errors"].keys())
 
-    output = {}
+    results = {}
     for scorer, classifier in product(scorers, classifiers):
-        scores = results["scores"][scorer]
-        errors = results["errors"][classifier]
+        scores = raw_results["scores"][scorer]
+        errors = raw_results["errors"][classifier]
         corr, p = pearsonr(scores, errors)
         if isnan(corr):
             corr = 0.0
         x = numpy.sort(scores)
         y = errors[scores.argsort()]
-        x_time = results["score_times"][scorer].mean()
-        y_time = results["classifier_times"][classifier].mean()
-        output[(scorer, classifier)] = {
+        x_time = raw_results["score_times"][scorer].mean()
+        y_time = raw_results["classifier_times"][classifier].mean()
+        results[(scorer, classifier)] = {
             "x": x,
             "y": y,
             "corr": corr,
@@ -94,29 +97,7 @@ def prepare_results(results):
             "x_time": x_time,
             "y_time": y_time
         }
-    return output
-        #     pyplot.plot(
-        #         sorted(scores),
-        #         errors[scores.argsort()],
-        #         STYLES[i % len(STYLES)],
-        #         linewidth=1.5,
-        #         markersize=6
-        #     )
-        # # Plot style
-        # x_range = pyplot.xlim()[1] - pyplot.xlim()[0]
-        # y_range = pyplot.ylim()[1] - pyplot.ylim()[0]
-        # pyplot.ylim(pyplot.ylim()[0], pyplot.ylim()[0]+1.5*y_range)
-        # y_range *= 1.5
-        # pyplot.axes().set_aspect(x_range / y_range)
-        # pyplot.grid()
-        # pyplot.xticks(numpy.linspace(*pyplot.xlim(), num=6))
-        # pyplot.yticks(numpy.linspace(*pyplot.ylim(), num=6))
-        # pyplot.legend([LEGEND[c] for c in classifiers])
-        # pyplot.xlabel("$\eta^2_\Lambda$")
-        # pyplot.ylabel("error")
-        #
-        # pyplot.savefig("figures/{}.pdf".format(figure_file_name), bbox_inches="tight")
-        # pyplot.close()
+    return results
 
 
 def get_figure_file_name(results_file_name, scorer):
@@ -125,7 +106,7 @@ def get_figure_file_name(results_file_name, scorer):
 
 def plot_correlation_to_range(all_results, pyplot):
     n_bins = 5
-    width = 0.8/5
+    width = 0.6/n_bins
 
     range_to_corr = defaultdict(lambda: defaultdict(list))
     for (dataset, normalize, subset_size), results in all_results.iteritems():
@@ -142,19 +123,21 @@ def plot_correlation_to_range(all_results, pyplot):
     for (scorer, classifier, normalize), data in range_to_corr.iteritems():
         normalize_format = "norm" if normalize else "orig"
         figure_name = "corr_to_range_{}_{}_{}".format(scorer, classifier, normalize_format)
+
         significant, bins, _ = pyplot.hist(data["significant"], bins=n_bins, range=(0, 1))
         valid, bins, _ = pyplot.hist(data["valid"], bins=n_bins, range=(0, 1))
         pyplot.close()
 
-        print "{}: {} significant ({} total)".format(figure_name, significant.sum(), valid.sum())
+        print "{}: {:.0f} significant ({:.0f} total)".format(figure_name, significant.sum(), valid.sum())
 
+        # Plot format
         x_ticks_pos = numpy.asarray([0.5*(bins[i] + bins[i+1]) for i in range(len(bins)-1)])
         left = x_ticks_pos - width/2
         pyplot.bar(left, 100 * (significant / valid), width=width)
 
         pyplot.ylim(0, 105)
-        pyplot.yticks(numpy.linspace(10, 100, 10))
-        pyplot.ylabel("Significant rankings (%)".format(LEGEND[classifier]))
+        pyplot.yticks(range(10, 101, 10), ["{}%".format(percent) for percent in range(10, 101, 10)])
+        pyplot.ylabel("Significant rankings".format(LEGEND[classifier]))
 
         x_ticks = ["{:.1f} to {:.1f}".format(bins[i], bins[i+1]) for i in range(len(bins)-1)]
         pyplot.xlim(0, 1)
@@ -169,6 +152,59 @@ def plot_correlation_to_range(all_results, pyplot):
         )
 
         pyplot.savefig("figures/{}.pdf".format(figure_name))
+        pyplot.close()
+
+
+def plot_correlation(all_results, pyplot):
+    datasets = dataset_names()
+    normalization = [True]
+    subset_sizes = [3, 5, 10, 15, 20]
+
+    for dataset, normalize, subset_size in product(datasets, normalization, subset_sizes):
+        results = all_results.get((dataset, normalize, subset_size), None)
+        if results is None:
+            continue
+        scorers = ["deter", "wilks", "uncer"]
+        classifiers = ["ab", "gb", "ba", "rf", "et"]
+        for i, scorer in enumerate(scorers):
+            for j, classifier in enumerate(classifiers):
+                data = results[(scorer, classifier)]
+                x = data["x"]
+                y = data["y"]
+                if x.min() <= 0 or x.max() >= 1:
+                    continue
+                if data["p"] > 0.02 or data["corr"] > 0:
+                    if numpy.random.choice([True, False], p=[0.95, 0.05]):
+                        continue
+                    else:
+                        wrong = True
+                else:
+                    wrong = False
+                pyplot.scatter(x, y, color=SCATTER_COLORS[j], marker=SCATTER_MARKERS[j], s=20)
+                # noinspection PyTupleAssignmentBalance
+                m, q = numpy.polyfit(x, y, deg=1)
+                x_min, x_max = pyplot.xlim()
+                pyplot.xlim(max(0, x_min), min(1, x_max))
+                pyplot.plot([x_min, x_max], [q + m*x_min, q + m*x_max], LINE_STYLES[j], linewidth=1.5)
+                # Format
+                pyplot.ylim(pyplot.ylim()[0], pyplot.ylim()[1])
+                y_range = pyplot.ylim()[1] - pyplot.ylim()[0]
+                x_range = pyplot.xlim()[1] - pyplot.xlim()[0]
+                pyplot.axes().set_aspect(x_range / y_range)
+                pyplot.grid()
+                pyplot.xticks(numpy.linspace(*pyplot.xlim(), num=6))
+                pyplot.yticks(numpy.linspace(*pyplot.ylim(), num=6))
+                pyplot.xlabel(LEGEND[scorer])
+                pyplot.ylabel("{} error rate".format(LEGEND[classifier]))
+                pyplot.title("{}, {} features - correlation: ${:.2f}$".format(
+                    dataset, subset_size,
+                    results[(scorer, classifier)]["corr"])
+                )
+                pyplot.savefig("figures/{}plot_{}_{}_{}.pdf".format(
+                    "wrong" if wrong else "",
+                    scorer, classifier, get_results_file_name(dataset, normalize, subset_size)), bbox_inches="tight"
+                )
+                pyplot.close()
 
 
 if __name__ == "__main__":
